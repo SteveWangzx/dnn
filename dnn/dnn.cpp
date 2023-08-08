633a3d1ad589b21a8f92252fca8e1ef426728d5e
SteveWangzx <stevezxwang@gmail.com>
2023-08-08 17:44:00 +0800

    添加项目文件。

---
+++ dnn/dnn.cpp
@@ -0,0 +1,19 @@
+﻿// 库导入
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+// 头文件
+#include "./dateCollector/dataCollector.hpp"
+
+int main()
+{
+	dataCollector test("./smoke_detection_iot.csv");
+	test.collectData();
+	test.peak(10);
+	test.mean_normalization();
+	test.peak(10);
+}
